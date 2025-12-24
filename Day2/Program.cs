51ad6f30b10349870b7d32769848637d3d19af75
Edward Burt <122280720+EdBurtCoupa@users.noreply.github.com>
2025-12-24 21:10:19 +0000

    learned about span

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,20 +1,33 @@
-﻿using var reader = new StreamReader(@".\egIn.csv");
+﻿using System.Diagnostics;
+
+using var reader = new StreamReader(@".\in.csv");
 
 string line = await reader.ReadToEndAsync();
 string[] rangeArray = line.Split(',');
+long runningTotal = 0L;
 
 foreach (string range in rangeArray)
 {
-    Console.WriteLine(range);
+    // Console.WriteLine(range);
 
     string[] idArray = range.Split('-');
+    long lowerBound = long.Parse(idArray[0]);
+    long upperBound = long.Parse(idArray[1]);
 
-    foreach (string id in idArray)
+    for (long i = lowerBound; i <= upperBound; i++)
     {
-        Console.WriteLine(id);
-        if (id[0] == id[1])
+        string id = i.ToString();
+        if (id.Length % 2 != 0)
         {
-            Console.WriteLine(string.Format(@"invalidId {0}", id));
+            continue;
+        }
+        int halfLength = id.Length / 2;
+        if (id.AsSpan(0, halfLength).SequenceEqual(id.AsSpan(halfLength, halfLength)))
+        {
+            runningTotal += i;
         }
     }
+
 }
+
+Console.WriteLine(runningTotal);
