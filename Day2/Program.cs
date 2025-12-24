cbbcaade834d2369290aaa2e4da93905387aced9
Edward Burt <122280720+EdBurtCoupa@users.noreply.github.com>
2025-12-24 17:55:27 +0000

    day2 wip out of time

---
+++ Day2/Program.cs
@@ -0,0 +1,20 @@
+﻿using var reader = new StreamReader(@".\egIn.csv");
+
+string line = await reader.ReadToEndAsync();
+string[] rangeArray = line.Split(',');
+
+foreach (string range in rangeArray)
+{
+    Console.WriteLine(range);
+
+    string[] idArray = range.Split('-');
+
+    foreach (string id in idArray)
+    {
+        Console.WriteLine(id);
+        if (id[0] == id[1])
+        {
+            Console.WriteLine(string.Format(@"invalidId {0}", id));
+        }
+    }
+}
