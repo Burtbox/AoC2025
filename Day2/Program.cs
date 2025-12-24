776c77a780eb6b02e1f630791ce92858000d4640
Edward Burt <122280720+EdBurtCoupa@users.noreply.github.com>
2025-12-24 21:42:05 +0000

    stick with good old strings

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -31,3 +31,37 @@ foreach (string range in rangeArray)
 }
 
 Console.WriteLine(runningTotal);
+
+
+long runningTotalB = 0L;
+foreach (string range in rangeArray)
+{
+    string[] idArray = range.Split('-');
+    long lowerBound = long.Parse(idArray[0]);
+    long upperBound = long.Parse(idArray[1]);
+
+    for (long i = lowerBound; i <= upperBound; i++)
+    {
+        string id = i.ToString();
+        for (int segmentLength = 1; segmentLength * 2 <= id.Length; segmentLength++)
+        {
+            if (id.Length % segmentLength != 0)
+            {
+                continue;
+            }
+            bool isRepetitive = true;
+            string initialSegment = id[..segmentLength];
+            for (int pos = segmentLength; pos < id.Length && isRepetitive; pos += segmentLength)
+            {
+                isRepetitive = initialSegment == id.Substring(pos, segmentLength);
+            }
+            if (isRepetitive)
+            {
+                runningTotalB += i;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(runningTotalB);
